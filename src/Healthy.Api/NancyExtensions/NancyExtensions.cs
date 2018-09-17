7d63912fd00d9a38c660a1677f202e15ace33dc8
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-09-17 22:22:03 +0200

    Added nancy base classes

---
+++ src/Healthy.Api/NancyExtensions/NancyExtensions.cs
@@ -0,0 +1,42 @@
+using Healthy.Infrastructure.Security;
+using Nancy;
+using Nancy.Authentication.Stateless;
+using Nancy.Bootstrapper;
+
+namespace Healthy.Api.NancyExtensions
+{
+    public static class NancyExtensions
+    {
+        public static object ToExceptionData(this NancyContext context)
+            => new
+            {
+                userId = context.CurrentUser?.Identity?.Name ?? string.Empty,
+                path = context.Request.Path,
+                method = context.Request.Method,
+                ipAddress = context.Request.UserHostAddress,
+                headers = context.Request.Headers
+            };
+
+        public static void SetupTokenAuthentication(this IPipelines pipelines, IJwtTokenHandler jwtTokenHandler)
+            => StatelessAuthentication.Enable(pipelines, Configuration(jwtTokenHandler));            
+
+        private static StatelessAuthenticationConfiguration Configuration(IJwtTokenHandler jwtTokenHandler) 
+            => new StatelessAuthenticationConfiguration(ctx =>
+            {
+                var authToken = jwtTokenHandler.GetFromAuthorizationHeader(ctx.Request.Headers.Authorization);
+                if(authToken.HasNoValue)
+                {
+                    return null;
+                }
+                var jwt = jwtTokenHandler.Parse(authToken.Value);
+                if(jwt.HasNoValue)
+                {
+                    return null;
+                }
+                var token = jwt.Value;
+
+                return jwt.HasValue ? new HealthyIdentity(token.Subject, 
+                    token.Role, token.State, token.Claims) : null;
+            });
+    }
+}
