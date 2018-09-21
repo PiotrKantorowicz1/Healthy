1f841d17186a1f4331b561c8fa931ca73d3ac5cf
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-09-22 01:30:40 +0200

    Added build scripts

---
+++ scripts/dotnet-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RUN="dotnet run"
+
+cd src
+cd healthy.api
+
+	 echo ========================================================
+	 echo Starting application in local environment
+	 echo ========================================================
+
+$RUN
