236e9f7f5b317b7431251660b1850bcb8eb8a4e2
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-09-29 14:02:47 +0200

    Tested domain aggregates for exception invoke

--- tests/Healthy.Tests/Diets/Domain/DomainTests.cs
+++ tests/Healthy.Tests/Diets/Domain/DomainTests.cs
@@ -20,7 +20,7 @@ namespace Healthy.Tests.Diets.Domain
         [Fact]
         public void Set_category_name_should_be_raise_error_when_category_name_is_not_set()
         {
-            Action act = () => Category.SetName("");
+            Action act = () => Category.SetName(string.Empty);
 
             var ex = Assert.Throws<DomainException>(act);
             Assert.Equal(ErrorCodes.InvalidCategory, ex.Code);
@@ -106,6 +106,105 @@ namespace Healthy.Tests.Diets.Domain
 
             var ex = Assert.Throws<DomainException>(act);
             Assert.Equal(ErrorCodes.ProductNotFound, ex.Code);
+        }
+
+        [Fact]
+        public void Set_name_should_be_raise_error_when_name_is_empty()
+        {
+            Action act = () => Product.SetName(string.Empty);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.NameNotProvided, ex.Code);
+        }
+
+        [Fact]
+        public void Set_namet_should_be_raise_error_when_name_is_too_long()
+        {
+            Action act = () => Product.SetName(@"YXNkaW9manNpYXVoZ2Zpb3Nwa29pZHNoZ29wc2tmZG9pa
+                HNqYWlnb2thamRzb2lnZmhqcGFqYTlbcGZqYXNkZ2lhc2RqZmdwW2FzamdkOWFpc1tkZ2ZoYXNnZmlh
+                c2RmZ2FzaWRnZmg5MGFzcGRvZ2FzZmdrZGE=");
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.InvalidProductName, ex.Code);
+        }
+
+
+        [Fact]
+        public void Set_description_should_be_raise_error_when_description_is_empty()
+        {
+            Action act = () => Product.SetDescription(string.Empty);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.DescriptionNotProvided, ex.Code);
+        }
+
+        [Fact]
+        public void Set_description_should_be_raise_error_when_description_is_too_long()
+        {
+            Action act = () => Product.SetDescription(@"YXNkaW9manNpYXVoZ2Zpb3Nwa29pZHNoZ29wc2tmZG9pa
+                HNqYWlnb2thamRzb2lnZmhqcGFqYTlbcGZqYXNkZ2lhc2RqZmdwW2FzamdkOWFpc1tkZ2ZoYXNnZmlh
+                c2RmZ2FzaWRnZmg5MGFzcGRvZ2FzZmdrZGE=XNkaW9manNpYXVoZ2Zpb3Nwa29pZHNoZ29wc2tmZG9pa
+                HNqYWlnb2thamRzb2lnZmhqcGFqYTlbcGZqYXNkZ2lhc2RqZmdwW2FzamdkOWFpc1tkZ2ZoYXNnZmlh
+                c2RmZ2FzaWRnZmg5MGFzcGRvZ2FzZmdrZGE=XNkaW9manNpYXVoZ2Zpb3Nwa29pZHNoZ29wc2tmZG9pa
+                HNqYWlnb2thamRzb2lnZmhqcGFqYTlbcGZqYXNkZ2lhc2RqZmdwW2FzamdkOWFpc1tkZ2ZoYXNnZmlh
+                c2RmZ2FzaWRnZmg5MGFzcGRvZ2FzZmdrZGE=");
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.InvalidDescription, ex.Code);
+        }
+
+        [Fact]
+        public void Set_quantity_should_be_raise_error_when_quantity_is_less_than_zero()
+        {
+            Action act = () => Product.SetQuantity(-1);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.InvalidQuantity, ex.Code);
+        }
+
+        [Fact]
+        public void Set_quantity_should_be_raise_error_when_quantity_is_greather_than_500()
+        {
+            Action act = () => Product.SetQuantity(501);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.InvalidQuantity, ex.Code);
+        }
+
+        [Fact]
+        public void Set_nutrition_values_should_be_raise_error_when_nutrition_values_is_null()
+        {
+            Action act = () => Product.SetNutritionValue(null);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.NutritionValuesNotProvided, ex.Code);
+        }
+
+        [Fact]
+        public void Set_category_should_be_raise_error_when_category_is_null()
+        {
+            Action act = () => Product.SetCategory(null);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.CategoryNotProvided, ex.Code);
+        }
+
+        [Fact]
+        public void Set_interval_should_be_raise_error_when_interval_is_null()
+        {
+            Action act = () => Supplementation.SetInterval(null);
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.IntervalNotProvided, ex.Code);
+        }
+
+        [Fact]
+        public void Get_daily_supplementation_should_be_raise_error_when_daily_supplementation_not_found()
+        {
+            Action act = () => Supplementation.GetDailySupplementationOrFail(Guid.NewGuid());
+
+            var ex = Assert.Throws<DomainException>(act);
+            Assert.Equal(ErrorCodes.DailySupplementationNotFound, ex.Code);
         }
     }
 }
