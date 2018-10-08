d7cd63c3b459e0d5300466d8c9e7af271ce2fd39
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-10-08 22:56:17 +0200

    Added IsValid methos to static string classes

--- src/Healthy.Core/Domain/Workouts/DomainClasses/WorkoutSubType.cs
+++ src/Healthy.Core/Domain/Workouts/DomainClasses/WorkoutSubType.cs
@@ -1,8 +1,22 @@
+using Healthy.Core.Domain.BaseClasses;
+
 namespace Healthy.Core.Domain.Workouts.DomainClasses
 {
-    public class WorkoutSubType
+    public static class WorkoutSubType
     {
         public const string Split = "Split";
         public const string FBW = "Full Body Workout";
+
+        public static bool IsValid(string workoutSubType)
+        {
+            if (string.IsNullOrWhiteSpace(workoutSubType))
+            {
+                return false;
+            }
+
+            workoutSubType = workoutSubType.ToLowerInvariant();
+
+            return workoutSubType == Split || workoutSubType == FBW;
+        }
     }
 }
--- src/Healthy.Core/Domain/Workouts/DomainClasses/WorkoutType.cs
+++ src/Healthy.Core/Domain/Workouts/DomainClasses/WorkoutType.cs
@@ -5,5 +5,18 @@ namespace Healthy.Core.Domain.Workouts.DomainClasses
         public const string Jogging = "Jogging";
         public const string Gym = "Gym";
         public const string Other = "Other";
+
+        public static bool IsValid(string workoutType)
+        {
+            if (string.IsNullOrWhiteSpace(workoutType))
+            {
+                return false;
+            }
+
+            workoutType = workoutType.ToLowerInvariant();
+
+            return workoutType == Jogging || workoutType == Gym
+                                          || workoutType == Other;
+        }
     }
 }
